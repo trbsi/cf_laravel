2691bef830cce8e615476536af2d8fd99c94b4be
Dario Trbovic <timmy.dario@gmail.com>
2016-05-13 09:27:29 +0200

    1st push

---
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Auth;
+/**
+ * Class FrontendController
+ * @package App\Http\Controllers
+ */
+class FrontendController extends Controller
+{
+    /**
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        var_dump(Auth::user()->id);
+        javascript()->put([
+            'test' => 'it works!',
+        ]);
+
+        return view('frontend.index');
+    }
+
+    /**
+     * @return \Illuminate\View\View
+     */
+    public function macros()
+    {
+        return view('frontend.macros');
+    }
+}
